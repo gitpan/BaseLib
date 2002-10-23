a9b2a76cbff68ec4edecaf902c640022b2acecc6
Hasanuddin Tamir <hasantamir@yahoo.com>
2002-10-23 06:16:17 +0000

    Import of HASANT/BaseLib-0.05 from CPAN.
    gitpan-cpan-distribution: BaseLib
    gitpan-cpan-version:      0.05
    gitpan-cpan-path:         HASANT/BaseLib-0.05.tar.gz
    gitpan-cpan-author:       HASANT
    gitpan-cpan-maturity:     released

--- BaseLib.pm
+++ BaseLib.pm
@@ -1,46 +1,59 @@
 package BaseLib;
 
-## $Id: BaseLib.pm,v 1.4 2001/02/27 14:11:05 hasant Exp $
+## $Id: BaseLib.pm,v 1.6 2002/10/23 12:04:31 hasant Exp $
 ## Manipulate @INC dynamically for independent installation path
 ##
-## Hasanuddin Tamir <hasant@trabas.com>
+## Written by: Hasanuddin Tamir <hasant@trabas.com>
 ## Copyright (C) 2000 Trabas.  All rights reserved.
 ##
-## This software is freeware.  You may redistribute it
+## This program is free software; you may redistribute it
 ## and/or modify it under the same terms as Perl itself.
 
 use strict;
+use File::Spec;
+use File::Basename 'dirname';
+use Cwd 'abs_path';
 
-$BaseLib::VERSION = '0.03';
+$BaseLib::VERSION = '0.05';
 
 sub import {
-	my $package = shift;
-
-	# FindBin provides full path to directory the script is run from
-	require FindBin;
-
-	# untaint the path supplied by FindBin module
-	croak("Invalid characters in `$FindBin::Bin'")
-		unless ($BaseLib::BaseDir = $FindBin::Bin) =~ s{^([\w/.,-]+)$}{$1};
+	shift;
+	my($base, $lib, $glob) = @_;
 
 	# the base directory name is now mandatory
-	my $base = shift;
 	croak('No base directory supplied')
 		unless defined $base && $base ne '';
 
-	# Save the path up to basedir name and forget the rest
-	croak("Couldn't find the base `$base' in `$BaseLib::BaseDir'")
-		unless $BaseLib::BaseDir =~ s{^(.*/$base/).*}{$1}; # find the longest
+	my $dirname = abs_path(dirname $0);
+	my @dirs = File::Spec->splitdir($dirname);
+	while (@dirs) {
+		last if $dirs[-1] eq $base;
+		pop @dirs;
+	}
 
-	my $libdir = "$BaseLib::BaseDir/" . (shift || 'lib');
+	@dirs || croak("Couldn't find the base `$base' in `$dirname'");
+	my $basedir = File::Spec->catdir(@dirs);
 
-	# ask lib.pm to finish the job :-)
-	# well, assummed it's not loaded yet
-	# XXX: is it necessary to check this?
+	$lib = File::Spec->catdir(qw(lib perl))
+		unless defined $lib and $lib ne '' and $lib ne '-';
+	my $libdir = File::Spec->catdir($basedir, $lib);
+
+	# ask lib.pm to finish the job :-) well, assummed it's not loaded yet
+	# FIXME: is it necessary to check this?
 	my %inc = map { $_ => 1 } @INC;
 	unless ($inc{$libdir}) {
 		require lib;
 		lib::import('lib', $libdir);
+	}
+
+	# give them if they want it
+	if ($glob) {
+		no strict 'refs';
+		my $pack = caller;
+		${"$pack\:\:$glob"} = $basedir;
+	}
+	else {
+		$BaseLib::BaseDir = $basedir;
 	}
 }
 
@@ -50,9 +63,6 @@ sub croak {
 	Carp::croak(__PACKAGE__, ": @_");
 }
 
-# Preloaded methods go here.
-
-# Autoload methods go after =cut, and are processed by the autosplit program.
 
 1;
 
@@ -79,26 +89,26 @@ application to work, the scripts may need to say:
 
    use lib '/usr/local/pim/lib-perl';
 
-Someday, you need to move the application to, say,
-C</vhost/www.some.host.com>, so you need to change the use statement
-all over the scripts to:
+Someday, you need to move the application to, say, C</vhost/www.host.com>,
+so you need to change the use statement all over the scripts to:
 
    use lib '/vhost/www.some.host.com/pim/lib-perl';
 
 If you do this a lot and get tired of changing the line for every
-different installation path, or, you don't want to bother people
-using your application to cange the line to meet their conditions,
-then you might need this module.
+different installation path in every script, or, you don't want to
+bother people using your application to change the line to meet their
+conditions, then you might need this module.
 
 Now your scripts can say:
 
    use BaseLib qw(pim lib-perl);
 
-where C<pim> and C<lib-perl> are the BASEDIR and LIBDIR arguments respectively.
+where C<pim> and C<lib-perl> are the BASEDIR and LIBDIR arguments,
+respectively.
 
-So there's no need to worry about different installation layout,
-wherever the C<pim> base directory is put under. The BASEDIR
-argument is required while LIBDIR is optional (defaults to C<lib>).
+So there's no need to worry about different installation layout, wherever
+the C<pim> base directory is put under. The BASEDIR argument is mandatory
+while LIBDIR is optional (defaults to C<lib/perl>).
 
 As addition, you can use B<BaseLib>'s global package variable,
 B<$BaseDir> to refer to the full path to C<pim>. For example,
@@ -110,22 +120,42 @@ will print,
 
    Data dir: /usr/local/pim/data
 
-Yes, you must qualify it with the package name since the variable is not
-exportable. This may change in the future.
+You have to use the fully qualified package name since it's not
+exportable.
 
-=head1 NOTE
+You can use your own global variable by supplying the third argument
+in the use statement. For example,
 
-Please consider this module, the code and its interface, as in ALPHA stage.
-Expect any change in the future versions.
+   use BaseLib qw(pim lib-perl MyOwnBaseDir);
 
-=head1 CAVEATS
+So saying,
 
-I only tested it in the following environment:
+   print "Data dir: $MyOwnBaseDir/data\n";
 
-   * Perl 5.005_03
-   * Linux
+will result the same thing. If you use 'use strict;', which is a good
+thing, you need to explicitly declare it as global variable. Either,
 
-It's unlikely to work on systems don't recognize B</> as path separator.
+   use vars '$MyOwnBaseDir';
+
+or, if you use version 5.6 or later,
+
+   our $MyOwnBaseDir;
+
+If you want to use the default LIBDIR (C<lib/perl>), you can indicate
+so by using dash ("-").
+
+   use BaseLib qw(pim - MyOwnBaseDir);
+
+Or, just give an empty string,
+
+   use BaseLib ('pim', '', 'MyOwnBaseDir');
+
+
+=head1 NOTES
+
+I rewrote the implementation to make this module more reusable in
+different environment. No path resolving is hardcoded. Unfortunately, I
+have no any chance to test it.
 
 B<BaseLib> will find the last occurence of I<BASEDIR> string. If you mean
 the base directory as C</usr/local/myapp>, while the script uses the module
@@ -133,11 +163,7 @@ locates in C</usr/local/myapp/bin/sample/myapp/test/script.pl>, then the
 full path to the application base directory ends in
 C</usr/local/myapp/bin/sample/myapp>.
 
-In mod_perl environment there's a high risk for conflict on the
-C<$BaseLib::BaseDir> if B<BaseLib> is used by more than one application in
-the same host.
-
-But any improvements/suggestions for wider support are welcome. A simple
+Any improvements/suggestions for wider support are welcome. A simple
 comment on this module will do as well.
 
 =head1 AUTHOR
@@ -146,7 +172,7 @@ This module is written by Hasanuddin Tamir E<lt>hasant@trabas.comE<gt>.
 
 Copyright (C) 2000 B<Trabas>.  All rights reserved.
 
-This software is freeware.  You may redistribute it
+This program is free software; you can redistribute it
 and/or modify it under the same terms as Perl itself.
 
 =head1 SEE ALSO
--- t/t1.t
+++ t/t1.t
@@ -10,15 +10,23 @@ use vars '$lib_dir';
 BEGIN {
     use FindBin '$Bin';
     $lib_dir = "$Bin/../lib";
+
     mkdir $lib_dir, 0755
         or die "failed to create $lib_dir: $!";
     open LIB, ">$lib_dir/Empty.pm"
         or die "Failed to create $lib_dir/Empty.pm: $!";
     print LIB "package Empty;\n1\n";
     close LIB;
+
+    mkdir "$lib_dir/perl", 0755
+        or die "failed to create $lib_dir/perl: $!";
+    open LIB, ">$lib_dir/perl/Empty1.pm"
+        or die "Failed to create $lib_dir/perl/Empty1.pm: $!";
+    print LIB "package Empty1;\n1\n";
+    close LIB;
 }
 
-BEGIN { $| = 1; print "1..3\n"; }
+BEGIN { $| = 1; print "1..4\n"; }
 END {print "not ok 1\n" unless $loaded;}
 use BaseLib ("BaseLib-$BaseLib::VERSION", 'lib');
 use Empty;
@@ -28,9 +36,13 @@ print "ok 1\n";
 print 'not ' unless "$BaseLib::BaseDir/lib" eq $INC[0];
 print "ok 2\n";
 
-system 'perl', "$Bin/s/s.pl";
+system perl => "$Bin/s/s.pl";
 print 'not ' if $?;
 print "ok 3\n";
+
+system perl => "$Bin/s/t.pl";
+print 'not ' if $?;
+print "ok 4\n";
 
 ######################### End of black magic.
 
@@ -39,6 +51,8 @@ print "ok 3\n";
 # of the test code):
 
 END {
-unlink "$lib_dir/Empty.pm";
-rmdir $lib_dir;
+ unlink "$lib_dir/Empty.pm";
+ unlink "$lib_dir/perl/Empty1.pm";
+ rmdir "$lib_dir/perl";
+ rmdir $lib_dir;
 }
